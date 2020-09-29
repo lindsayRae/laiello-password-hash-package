d4182de599cb71b8321c36932c5b8e7d6260a88b
lindsayRae <lbarnett712@gmail.com>
2020-09-29 07:22:12 -0400

    feat - eslint commands and eslint on all files

--- src/password-hash.test.js
+++ src/password-hash.test.js
@@ -1,24 +1,20 @@
-
-import { hashPassword, verifyHash } from './password-hash'
+import { hashPassword, verifyHash } from './password-hash';
 
 describe('hashPassword basic functionality', () => {
+  const controlPass = hashPassword('testPassword');
+  test('verifyHash is true when I pass "testPassword" as the password', () => {
+    expect(verifyHash('testPassword', controlPass)).toBe(true);
+  });
 
-    const controlPass = hashPassword("testPassword")
-    test('verifyHash is true when I pass "testPassword" as the password', () => {
-        expect(verifyHash("testPassword", controlPass)).toBe(true)
-    }) 
+  test('verifyHash is false when I pass "badPassword" as the password', () => {
+    expect(verifyHash('badPassword', controlPass)).toBe(false);
+  });
 
-    test('verifyHash is false when I pass "badPassword" as the password', () => {
-        expect(verifyHash("badPassword", controlPass)).toBe(false)
-    }) 
+  test('verifyHash is false when I pass "" as the password', () => {
+    expect(verifyHash('', controlPass)).toBe(false);
+  });
 
-    test('verifyHash is false when I pass "" as the password', () => {
-        expect(verifyHash("", controlPass)).toBe(false)
-    }) 
-
-    test('verifyHash is false when I pass null as the password', () => {
-        expect(verifyHash(null, controlPass)).toBe(false)
-    }) 
-})
-
-
+  test('verifyHash is false when I pass null as the password', () => {
+    expect(verifyHash(null, controlPass)).toBe(false);
+  });
+});
